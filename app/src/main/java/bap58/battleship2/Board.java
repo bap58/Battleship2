52a85133a6bef2d1b98d9d2094378e4458474575
beatrizperret <bep26@georgetown.edu>
2017-11-14 20:58:31 -0500

    Structure of Board Class

--- app/src/main/java/bap58/battleship2/Board.java
+++ app/src/main/java/bap58/battleship2/Board.java
@@ -1,8 +1,49 @@
 package bap58.battleship2;
 
+import java.util.LinkedList;
+
 /**
  * Created by Brian on 11/13/2017.
+ * Beatriz is working on this 
  */
 
+//import statements
+import android.graphics.Canvas; //graphics
+
+//start implementing public class Board
 public class Board {
-}
+
+    BoardSquare[][] theSquares;
+    static int dimension = 10;
+    //LinkedList<Scorable> possibleWins;
+
+    public Board() {
+        //create board with randomly-placed ships
+        //create list of possible wins when player pushes play game
+    }
+
+    public void drawMe(Canvas g) {
+        //draw buttons
+        //draw 100 squares
+    }
+
+    public BoardSquare findSquare(){
+
+        BoardSquare square = null;
+        //get where player clicked
+        //is this necessary if it's a button
+
+        return square;
+    }
+
+    /*
+    Other functions worth writing: whose turn, winner, play, save file, load file,
+     */
+
+    // get function for member variable dimension
+    public static int getDimension() {return  dimension;}
+
+
+} //end implementing public class Board
+
+
