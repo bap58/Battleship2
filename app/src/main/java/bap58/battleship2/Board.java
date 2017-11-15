e6d6fcfabafee3f90971b103af54f15fa52d2327
beatrizperret <bep26@georgetown.edu>
2017-11-14 22:15:13 -0500

    Structure of Board Class

--- app/src/main/java/bap58/battleship2/Board.java
+++ app/src/main/java/bap58/battleship2/Board.java
@@ -15,10 +15,27 @@ public class Board {
 
     BoardSquare[][] theSquares;
     static int dimension = 10;
-    //LinkedList<Scorable> possibleWins;
+    static int[] sizes = {2, 3, 3, 4, 5};
+    boolean myBoard;
+    //on own board list ships that player will place
+    //on opponent board list is not drawn unless hit
+    LinkedList<Ship> ships;
 
-    public Board() {
-        //create board with randomly-placed ships
+    public Board(boolean mine) {
+        //create 100 squares
+        myBoard = mine;
+        theSquares = new BoardSquare[dimension][dimension];
+        for (int i = 0; i < dimension; i++){
+            for (int j = 0; j < dimension; j++) {
+                theSquares[i][j] = new BoardSquare();
+            }
+        }
+        if (mine) {
+            setShips(ships);
+        } else {
+            getShips();
+        }
+        //randomly place ships on personal board
         //create list of possible wins when player pushes play game
     }
 
@@ -42,6 +59,28 @@ public class Board {
 
     // get function for member variable dimension
     public static int getDimension() {return  dimension;}
+
+    public LinkedList<Ship> getShips() {
+        LinkedList<Ship> solutions = new LinkedList<Ship>();
+        return solutions;
+    }
+
+    public void setShips(LinkedList<Ship> ships) {
+        //randomly set ships
+        //for loop, one for each ship
+        for (int i = 0; i < sizes.length; i++){
+            int
+            Ship s = new Ship();
+        }
+    }
+
+    public void moveShip(){
+
+    }
+
+    public void rotateShip(){
+
+    }
 
 
 } //end implementing public class Board
