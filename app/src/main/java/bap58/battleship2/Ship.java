ee094e6a944883679c2a25f8eb7473423d85c630
JosephDelaney5 <Jad360@georgetown.edu>
2017-12-12 23:18:14 -0500

    Added some comments to the ship class

--- app/src/main/java/bap58/battleship2/Ship.java
+++ app/src/main/java/bap58/battleship2/Ship.java
@@ -7,11 +7,6 @@ import java.util.Random;
 import static bap58.battleship2.Board.dimension;
 import static bap58.battleship2.BoardSquare.squareSize;
 
-
-/**
- * Created by joe on 11/13/17.
- */
-
 public class Ship
 {
     int i;  //position i in the array
@@ -21,9 +16,9 @@ public class Ship
     int size; //Represents the amount of squares in shape
     //BoardSquare[] spots; //Represents the squares that make up the ship
     String orientation = "horizontal"; //Vertical or horizontal
-    int hitCount;
-    boolean selected;
-    boolean sunk;
+    int hitCount; //How many times this ship has been hit
+    boolean selected; //Flag to tell if ship is selected or not
+    boolean sunk; //Flag to tell if ship is sunk or not
 
     //constructor for Ship class
     public Ship(int i1, int j1, int s, String o)
@@ -41,20 +36,17 @@ public class Ship
         selected = false;
     }
 
-    //getters
+    //getters - Accessor functions
     int getI(){return i;}
     int getJ(){return j;}
-    int getX(){return x;}
-    int getY(){return y;}
     int getSize(){return size;}
     String getOrientation(){return orientation;}
-    int getHitCount(){return hitCount;}
     boolean getSelected(){return selected;}
     boolean getSunk() {return sunk;}
 
 
 
-    //setters
+    //setters - Mutator Functions
     void setI(int a)
     {
         i=a;
@@ -67,7 +59,6 @@ public class Ship
     }
     void setSize(int s) {size = s;}
     void setOrientation(String s){orientation = s;}
-    void incrementHitCount(){hitCount++;}
     void setSelected(boolean b){selected = b;}
     void setSunk(boolean b) {sunk = b;}
 
@@ -83,59 +74,48 @@ public class Ship
         {
             orientation = "vertical"; //change to vertical
         }
-        else
-        {
-            //can't do this. add a warning
-        }
+
+        //Cannot rotate a ship unless meets one of two conditions above
     }
 
 
-    public boolean isSunk()
-    {
-        return sunk;
-    }
-
-    //Need to figure out how to do this efficiently
-    //Moves the shape when the user moves the ship using his/her finger
-    public void move(int i1, int j1)
-    {
-        if(!isOffEdge(orientation, size, i1, j1)) {
-            i = i1;
-            j = j1;
-        }
-        else
-        {
-            //ERROR! moved it off the edge
-        }
-    }
-
+    //This function will take in an orientation, size, i and j
+    //It checks to see if a ship with specified values will go off the edge of the board
+    //Returns true if ship goes off edge
+    //Returns false if ship remains on the board
     public boolean isOffEdge(String orientation1, int size1, int i1, int j1)
     {
         boolean answer = false;
 
-        if(orientation1.equals("horizontal"))
+        if(orientation1.equals("horizontal")) //If orientation is horizontal
         {
+            //If size of ship plus its starting i position is greater than width of board
             if(size1 + i1 > dimension)
             {
-                answer = true;
+                answer = true; //Then the ship is off the edge, set answer to true
             }
         }
         else    //if vertical
         {
+            //If ship size + starting j position is greater than length of the board
             if(size1 + j1 > dimension)
             {
-                answer = true;
+                answer = true; //Then the ship is off the edge, set answer to true
             }
         }
 
-        return answer;
+        return answer; //return the answer
     }
 
 
+    //This function turns a ship object into a String
+    //Will be used to send ships over the server
     public String toString()
     {
         String returnString = "";
 
+        //Format: Board i j orientation size
+        //Example: Board 2 2 horizontal 3
         returnString = "Board " + i + " " + j + " " + orientation + " " + size;
 
         return returnString;
